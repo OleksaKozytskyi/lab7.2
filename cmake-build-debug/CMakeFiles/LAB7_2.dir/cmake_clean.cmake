04d2486f7a548e1e0f4b5648cfebce0d12a4f3a5
Oleksa Kozytskyy <askello_ua@MacBook-Air-Oleksa.local>
2021-01-13 17:17:07 +0200

    help

---
+++ cmake-build-debug/CMakeFiles/LAB7_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAB7_2.dir/main.cpp.o"
+  "LAB7_2"
+  "LAB7_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LAB7_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
